9085c5279054585e475cbe774c18b1bdede5c26c
BrunnoM7 <brunno.marchetti7@gmail.com>
2022-03-26 19:59:21 -0300

    fix: linter issues fixed

--- src/main/index.tsx
+++ src/main/index.tsx
@@ -1,8 +1,8 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import React from 'react'
+import ReactDOM from 'react-dom'
 import Login from '@/presentation/pages/login/login'
 
 ReactDOM.render(
   <Login />,
-  document.getElementById(`main`)
+  document.getElementById('main')
 )
