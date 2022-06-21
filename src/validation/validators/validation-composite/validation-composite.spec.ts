a6a61fa582793c296eff8d9d3db95f98cef840b9
BrunnoM7 <brunno.marchetti7@gmail.com>
2022-06-21 08:25:25 -0300

    test: ensure ValidationComposite returns falsy if there is no error

--- src/validation/validators/validation-composite/validation-composite.spec.ts
+++ src/validation/validators/validation-composite/validation-composite.spec.ts
@@ -1,13 +1,14 @@
 import { FieldValidationSpy } from '../test/mock-field-validation'
 import { ValidationComposite } from './validation-composite'
+import faker from '@faker-js/faker'
 
 type SUTtypes = {
   sut: ValidationComposite
   fieldValidationsSpy: FieldValidationSpy[]
 }
 
-const makeSut = (): SUTtypes => {
-  const fieldValidationsSpy = [new FieldValidationSpy('any_field'), new FieldValidationSpy('any_field')]
+const makeSut = (fieldName: string): SUTtypes => {
+  const fieldValidationsSpy = [new FieldValidationSpy(fieldName), new FieldValidationSpy(fieldName)]
   const sut = new ValidationComposite(fieldValidationsSpy)
 
   return {
@@ -18,11 +19,21 @@ const makeSut = (): SUTtypes => {
 
 describe('ValidationComposite', () => {
   test('Should return error if any validation fails', () => {
-    const { sut, fieldValidationsSpy } = makeSut()
-    fieldValidationsSpy[0].error = new Error('first_error_message')
-    fieldValidationsSpy[1].error = new Error('second_error_message')
-    const error = sut.validate('any_field', 'any_value')
+    const fieldName = faker.database.column()
+    const { sut, fieldValidationsSpy } = makeSut(fieldName)
+    const errorMessage = faker.random.words()
+    fieldValidationsSpy[0].error = new Error(errorMessage)
+    fieldValidationsSpy[1].error = new Error(faker.random.words())
+    const error = sut.validate(fieldName, faker.random.words())
 
-    expect(error).toBe('first_error_message')
+    expect(error).toBe(errorMessage)
+  })
+
+  test('Should return falsy if there is no error', () => {
+    const fieldName = faker.database.column()
+    const { sut } = makeSut(fieldName)
+    const error = sut.validate(fieldName, faker.random.words())
+
+    expect(error).toBeFalsy()
   })
 })
