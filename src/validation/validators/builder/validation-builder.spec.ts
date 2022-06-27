45486bf23c92008b4ab78b741d31ae0f81e9b5d1
BrunnoM7 <brunno.marchetti7@gmail.com>
2022-06-27 17:21:32 -0300

    test: ensure ValidationBuilder returns a list of validations

--- src/validation/validators/builder/validation-builder.spec.ts
+++ src/validation/validators/builder/validation-builder.spec.ts
@@ -1,22 +1,35 @@
 import { RequiredFieldValidation, EmailValidation, MinLengthValidation } from '@/validation/validators'
+import faker from '@faker-js/faker'
 import { ValidationBuilder as sut } from './validation-builder'
 
 describe('ValidationBuilder', () => {
   test('Should return RequiredFieldValidation', () => {
-    const validations = sut.field('any_field').required().build()
+    const field = faker.database.column()
+    const validations = sut.field(field).required().build()
 
-    expect(validations).toEqual([new RequiredFieldValidation('any_field')])
+    expect(validations).toEqual([new RequiredFieldValidation(field)])
   })
 
   test('Should return EmailValidation', () => {
-    const validations = sut.field('any_field').email().build()
+    const field = faker.database.column()
+    const validations = sut.field(field).email().build()
 
-    expect(validations).toEqual([new EmailValidation('any_field')])
+    expect(validations).toEqual([new EmailValidation(field)])
   })
 
   test('Should return MinLengthValidation', () => {
-    const validations = sut.field('any_field').min(5).build()
+    const field = faker.database.column()
+    const number = faker.datatype.number()
+    const validations = sut.field(field).min(number).build()
 
-    expect(validations).toEqual([new MinLengthValidation('any_field', 5)])
+    expect(validations).toEqual([new MinLengthValidation(field, number)])
+  })
+
+  test('Should return a list of validations', () => {
+    const field = faker.database.column()
+    const number = faker.datatype.number()
+    const validations = sut.field(field).required().min(number).email().build()
+
+    expect(validations).toEqual([new RequiredFieldValidation(field), new MinLengthValidation(field, number), new EmailValidation(field)])
   })
 })
